4d359218f26106dd47ddf0039b53a83f2dd62df8
ChenemiKen <chenemiken15@gmail.com>
2025-03-27 06:30:17 +0100

    change address to be bech32

--- submission/02.sh
+++ submission/02.sh
@@ -1,2 +1,3 @@
 # Create a new Bitcoin address, for receiving change.
-CHANGE_ADDRESS=$(bitcoin-cli -regtest  -rpcwallet=btrustwallet getnewaddress -addresstype legacy)
+CHANGE_ADDRESS=$(bitcoin-cli -regtest  -rpcwallet=btrustwallet getnewaddress -addresstype bech32)
+echo $CHANGE_ADDRESS
