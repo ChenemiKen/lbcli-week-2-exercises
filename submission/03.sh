41e6b3838118a3b6c7413eac6c960ef76473e573
ChenemiKen <chenemiken15@gmail.com>
2025-03-27 06:38:39 +0100

    fix 03 remove quotes

--- submission/03.sh
+++ submission/03.sh
@@ -2,7 +2,7 @@
 SEGWIT_ADDRESS=$(bitcoin-cli -regtest  -rpcwallet=btrustwallet getnewaddress -addresstype bech32)
 
 # Add funds to the address.
-bitcoin-cli -regtest generatetoaddress 101 "$SEGWIT_ADDRESS"
+bitcoin-cli -regtest generatetoaddress 101 $SEGWIT_ADDRESS
 
 # Return only the Address
 echo $SEGWIT_ADDRESS
