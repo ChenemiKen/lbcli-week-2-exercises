a8e0160dffee9e8ee8e96ed1829fbf28eca59701
ChenemiKen <chenemiken15@gmail.com>
2025-03-27 05:31:57 +0100

    solve 1 to 5

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # Create a new Bitcoin address, for receiving change.
+CHANGE_ADDRESS=$(bitcoin-cli -regtest  -rpcwallet=btrustwallet getnewaddress -addresstype legacy)
+echo "change address: $CHANGE_ADDRESS"
--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,8 @@
 # Created a SegWit address.
+SEGWIT_ADDRESS=$(bitcoin-cli -regtest  -rpcwallet=btrustwallet getnewaddress -addresstype bech32)
+
 # Add funds to the address.
+bitcoin-cli -regtest generatetoaddress 101 "$SEGWIT_ADDRESS"
+
 # Return only the Address
+echo $SEGWIT_ADDRESS
