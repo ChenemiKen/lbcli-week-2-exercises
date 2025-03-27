4d1318d47b3cdfbf2256bb37863c29216723581b
ChenemiKen <chenemiken15@gmail.com>
2025-03-27 06:19:54 +0100

    fix 02

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,2 @@
 # Create a new Bitcoin address, for receiving change.
 CHANGE_ADDRESS=$(bitcoin-cli -regtest  -rpcwallet=btrustwallet getnewaddress -addresstype legacy)
-echo "change address: $CHANGE_ADDRESS"
